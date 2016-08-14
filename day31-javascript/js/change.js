5fc397a5c1398502966b049d2b7903b95190d58f
sharryhong <kshopzoa15@gmail.com>
2016-08-14 20:24:08 +0900

    라이언 갤러리. 주석 추가
    추후 sass로 아리따운 갤러리 만들지어다.. 넵!

--- day31-javascript/js/change.js
+++ day31-javascript/js/change.js
@@ -1,23 +1,32 @@
-/*******************************
-* 라이언 갤러리   
+/**********************************
+* 라이언 갤러리 @ Hong HS. 2016. 08
+* html에서 button을 추가 하여도 작동하도록 만들기 
 */
 
-// 바뀌어야 하는 이미지 
-var main_img = query('.main-con img');
-var main_img_src = main_img.getAttribute('src');
-var img_array = ['img/img01.jpg', 'img/img02.jpg', 'img/img03.jpg'];
+// 바뀌어야 하는 img요소 담는 변수 만들기
+var main_img = query('.gallery img');
 
+// buttons 변수 : 각 button 요소를 담고 있는 배열. 아래 for문에서 만듭니다. 
 var buttons = [];
 
-function changeImg() {
-	main_img.setAttribute('src', this.img_src);
-	console.log('click');
-}
+// button요소가 몇개인가 알기 위해
+// btn_length에는 [<button>, <button>, ...] 유사배열이 생성됩니다. 
+var btn_length = queryAll('.indicator button');
+console.log(btn_length.length);
 
-for(var i = 0, j = img_array.length; i < j; i++){
+// this란 각 버튼 buttons[i] (context)을 말하며, 
+// 각 버튼 buttons[i]의 객체 프로퍼티 img_src 값을, main_img변수의 src속성 값으로 넣어줍니다. 
+var changeImg = function() {
+	main_img.setAttribute('src', this.img_src);
+};
+
+// j = btn_length.length // button요소의 갯수만큼 for문을 돌립니다.
+for(var i = 0, j = btn_length.length; i < j; i++){
+	// buttons변수에 각 button 요소 담기(배열)
 	buttons[i] = query('.indicator li:nth-child('+(i+1)+') button');
-	buttons[i].img_src = img_array[i];
-	console.log(buttons[i].img_src);
+	// buttons변수 각 배열값에 img_src 객체 프로퍼티 만들기. 값은 src 주소 
+	buttons[i].img_src = "img/img"+(i+1)+".gif";
+	console.log(buttons[i].img_src); // 테스트용
+	// 각 버튼을 클릭하면 changeImg 함수 실행하기 
 	buttons[i].onclick = changeImg;
 }
-
